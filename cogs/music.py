d7bbfa9037ff8c22d6880700421d151f24bd5df0
justnoted <just.noted3505@gmail.com>
2025-01-01 03:16:44 -0600

    Setup for music.py cog

---
+++ cogs/music.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class Music(commands.Cog, description="<a:catmusic:1238961718474838036>"):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.client.tree.sync()
+        print(f"{__name__} is ready.")
+
+
+async def setup(client):
+    await client.add_cog(Music(client))
