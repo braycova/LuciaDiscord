5af274e0558b8f1b07f22e67b3588ffa38514a18
justnoted <just.noted3505@gmail.com>
2025-05-12 02:46:27 -0500

    Updated database.py, ignored config.json

--- data/database.py
+++ data/database.py
@@ -1,10 +1,11 @@
+import json
 import os
 import pymongo
 from dotenv import load_dotenv
 from random import randint
 
 load_dotenv()
-client = pymongo.MongoClient(os.getenv("MONGO_DB"))
+client = pymongo.MongoClient(os.getenv("MONGODB_URL"))
 db = client['Lucia']
 
 calendar = db['calendar']
@@ -12,38 +13,42 @@ economy = db['economy']
 shop = db['shop']
 warnings = db['warnings']
 
+
 def get_random_response(response_type):
     doc = db['responses'].find_one({"type": response_type})
     responses = doc['responses']
     index = randint(0, len(responses) - 1)
     return responses[index], index
 
+
+def get_config_info(config_type):
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    return config[config_type]
+
+
 def get_economy_profile(user_id):
     user = economy.find_one({"userId": user_id})
     if not user:
-        new_user = {
-            "userId": user_id,
-            "balance": {
-                "wallet": 200,
-                "bank": 0,
-                "storage": 3000
-            },
-            "inventory": {
-                "Bank Note": 0
-            }
-        }
+        new_user = { "userId": user_id }
+        new_user.update(get_config_info("starting_econ_profile"))
         doc = economy.insert_one(new_user)
         user = economy.find_one({"userId": doc.inserted_id})
     return user
 
+
 def get_all_economy_profiles():
     return list(economy.find({"userId": {"$ne": "bank"}}))
+
 
 def get_all_shop_items():
     return list(shop.find().sort("price", pymongo.ASCENDING))
 
+
 def update_balance_value(user_id, field_name, value):
     economy.update_one({"userId": user_id}, {"$inc": {f"balance.{field_name}": value}})
+
 
 def update_inventory_value(user_id, item_name, value):
     economy.update_one({"userId": user_id}, {"$inc": {f"inventory.{item_name}": value}})
